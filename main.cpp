3d1a1826330e8b14cc2d7696c82550ea170e8e49
svenkis <sven.k.helland@outlook.com>
2025-04-04 16:31:18 +0200

    Deleted comments

--- main.cpp
+++ main.cpp
@@ -1,20 +1,6 @@
-//
-// This is example code from Chapter 2.2 "The classic first program" of
-// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
-//
-// This program outputs the message "Hello, World!" to the monitor
 
-#include "std_lib_facilities.h"
-
-//------------------------------------------------------------------------------'
-
-// C++ programs start by executing the function main
 int main() {
-    // Show a nice message in the Terminal window
-    cout << "Hello, World!" << endl;
 
     std::cout << "Hellohellohello!" << std::endl;
     return 0;
 }
-
-//------------------------------------------------------------------------------
