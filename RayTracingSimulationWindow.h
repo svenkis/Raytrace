ca74fd7cf9905875823ee8688e76b7f4e3cfac79
MSK0-10 <Modasir.kh02@gmail.com>
2025-04-08 01:05:45 +0200

    Added working raytracing for cricles.

--- RayTracingSimulationWindow.h
+++ RayTracingSimulationWindow.h
@@ -1,25 +1,24 @@
 #pragma once
 #include "std_lib_facilities.h"
 #include "AnimationWindow.h"
+#include "dynamicObjects.h"
+
 
 class RayTracingSimulationWindow : public AnimationWindow {
     private:
-    TDT4102::Point rayStart;
-    TDT4102::Point rayEnd;
-    TDT4102::Color rayColor;
-
-    double angle;
-        
-    public:
-    RayTracingSimulationWindow(int x = 50, int y = 50, int width = 1600, int height = 900, const std::string& title = "RayTracing Simulation Window");
-
-    bool collisionCircle(TDT4102::Point rayPoint, TDT4102::Point circleOrigin, double circleRadius);
-
-    bool collisionEdgeOfWindow(TDT4102::Point rayPoint, RayTracingSimulationWindow window);
-
-    void drawRay(TDT4102::Point rayStart, TDT4102::Point rayEnd, double angle, TDT4102::Color rayColor);
-
-    TDT4102::Point calculateRayEndPoint(TDT4102::Point rayStart, double angle);
-
+    std::vector<Circle> circles;
+    std::vector<Ray> rays;
     
+       
+    public:
+    RayTracingSimulationWindow(int x = 50, int y = 50, int width = 1000, int height = 1000, const std::string& title = "RayTracing Simulation Window");
+
+    void addCircle(const TDT4102::Point& origin, double radius);
+
+    TDT4102::Point traceRay( TDT4102::Point rayStart, double angle);
+
+    void drawRay(TDT4102::Point rayStart, double angle, TDT4102::Color rayColor);
+
+    void fillRays(TDT4102::Point rayStart, TDT4102::Color rayColor, int numRays);
+
     };
--- main.cpp
+++ main.cpp
@@ -8,16 +8,33 @@ int main() {
     RayTracingSimulationWindow window;
     window.setBackgroundColor(TDT4102::Color::black);
 
-    //circle
-    int radius = 25;
+    //light
+    int radius = 5;
     TDT4102::Color circleColor = TDT4102::Color::white;
+    int numRays = 500;
+
+
+    TDT4102::Point origin1;
+    origin1.x = 500;
+    origin1.y = 500;
+    window.addCircle(origin1, 100);
+
+    TDT4102::Point origin2;
+    origin2.x = 1000;
+    origin2.y = 500;
+    window.addCircle(origin2, 100);
+ 
 
     while(!window.should_close()) {
         TDT4102::Point mouseCoordinates = window.get_mouse_coordinates();
 
-        //if (window.is_left_mouse_button_down() != false){
+        
         window.draw_circle(mouseCoordinates,radius,circleColor);
-        //}
+        window.draw_circle(origin1,100,circleColor);
+        window.draw_circle(origin2,100,circleColor);
+
+        window.fillRays(mouseCoordinates,circleColor,numRays);
+        
 
         
 
